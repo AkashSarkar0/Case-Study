a279e781bee55ad043b22a02ef3265d3d604317b
Akash Sarkar <98975897+AkashSarkar0@users.noreply.github.com>
2022-06-04 01:32:13 +0530

    Add files via upload

---
+++ adminMicroservice/controller/AdminController.java
@@ -0,0 +1,52 @@
+package casestudy.adminMicroservice.controller;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import casestudy.adminMicroservice.model.Train;
+import casestudy.adminMicroservice.repository.TrainRepository;
+
+@CrossOrigin("http://localhost:3000")
+@RequestMapping("/admin")
+@RestController
+public class AdminController {
+	
+	    @Autowired
+	    TrainRepository trainRepository;
+
+	    @GetMapping("/train/getall")
+	    public ResponseEntity<?> gettrains(){
+	    return ResponseEntity.ok(this.trainRepository.findAll());
+	    }
+
+	    @GetMapping("/train/{id}")
+	        public ResponseEntity<?> getDetails(@PathVariable Integer id) {
+	       	 return ResponseEntity.ok(this.trainRepository.findById(id));
+	    }
+
+	    @PostMapping("/train/add")
+	    public ResponseEntity<?> addTrain(@RequestBody Train train) {
+	        	Train e=this.trainRepository.save(train);
+	        	return ResponseEntity.ok(e);
+	    }
+		
+		@PutMapping("/train/update")
+		public ResponseEntity<?> updateTrain(@RequestBody Train train) {
+			Train e=this.trainRepository.save(train);
+			return ResponseEntity.ok(e);
+		}
+		
+		@DeleteMapping("/train/{id}") 
+		public void deleteTrain(@PathVariable Integer id){
+			this.trainRepository.deleteById(id);
+		}
+
+	}
+
